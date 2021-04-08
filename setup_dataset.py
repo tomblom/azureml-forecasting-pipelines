811212e79c5ceb4d4090c58c3068640ea0e3a3d8
Tom Blomfield <tom@intranel.com>
2021-04-09 09:16:39 +1200

    Update setup_dataset so it saves full csv locally. This should be a speed up when interating on data transformation.
    .data folder has been added to git.

--- setup_dataset.py
+++ setup_dataset.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from datetime import datetime
+import pandas as pd
 from azureml.data import TabularDataset
 from azureml.data.azure_storage_datastore import AzureBlobDatastore
 from azureml.data.dataset_factory import TabularDatasetFactory
@@ -16,30 +17,37 @@ def main():
 
     ws = Workspace.from_config()
 
-    blob_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/nyc_energy.csv"
-    dataset: TabularDataset = TabularDatasetFactory.from_delimited_files(path=blob_path)\
-        .with_timestamp_columns(fine_grain_timestamp=time_column_name)
-
-    # Cut off the end of the dataset due to large number of nan values
-    dataset = dataset.time_before(datetime(2017, 10, 10, 5))
-
+    # Check if local data folder exists
     data_folder = '.data'
     os.makedirs(data_folder, exist_ok=True)
 
-    # split into train and test (based on time) and save to file locally
-    train = dataset.time_before(datetime(2017, 8, 8, 5), include_boundary=True)
-    train_df = train.to_pandas_dataframe()
-    train_df = train_df.reset_index()
-    train_df = train_df[[time_column_name, target_column_name]]
-    train_file_path = f"{data_folder}/train.csv"
-    train_df.to_csv(train_file_path)
+    # Check if dataset has been downloaded
+    full_file_path = f"{data_folder}/full.csv"
 
-    test = dataset.time_between(datetime(2017, 8, 8, 6), datetime(2017, 8, 10, 5))
-    test_df = test.to_pandas_dataframe()
-    test_df = test_df.reset_index()
-    test_df = test_df[[time_column_name, target_column_name]]
+    if os.path.exists(full_file_path):
+        df = pd.read_csv(full_file_path, parse_dates=[0])
+    else:
+        # download dataset
+        blob_path = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/nyc_energy.csv"
+        dataset: TabularDataset = TabularDatasetFactory.from_delimited_files(path=blob_path)\
+            .with_timestamp_columns(fine_grain_timestamp=time_column_name)
+
+        # Cut off the end of the dataset due to large number of nan values
+        dataset = dataset.time_before(datetime(2017, 10, 10, 5))
+
+        # Save full dataset
+        df = dataset.to_pandas_dataframe()
+        df = df.reset_index()
+        df = df[[time_column_name, target_column_name]]
+        df.to_csv(full_file_path, index=False)
+
+    train = df[df[time_column_name] <= datetime(2017, 8, 8, 5)]
+    train_file_path = f"{data_folder}/train.csv"
+    train.to_csv(train_file_path, index=False)
+
+    test = df[(df[time_column_name] >= datetime(2017, 8, 8, 6)) & (df[time_column_name] <= datetime(2017, 8, 10, 5))]
     test_file_path = f"{data_folder}/test.csv"
-    test_df.to_csv(test_file_path)
+    test.to_csv(test_file_path, index=False)
 
     # Upload data
     datastore: AzureBlobDatastore = ws.get_default_datastore()
@@ -53,7 +61,7 @@ def main():
 
     # Register dataset by name
     dataset_name = "nyc_energy_dataset_train"
-    dataset.register(workspace=ws, name=dataset_name)
+    dataset.register(workspace=ws, name=dataset_name, create_new_version=True)
 
     print("Dataset successfully registered")
 
